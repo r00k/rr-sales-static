2d1ee227e888c2d7a7a6d70f67ad5283efd3008b
Ben Orenstein <ben.orenstein@gmail.com>
2017-10-13 15:44:23 -0400

    Add existing landing page

--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ require 'sinatra'
 get "/buy" do
   send_file File.expand_path('index.html', settings.public_folder)
 end
+
+get "/landing" do
+  send_file File.expand_path('landing/index.html', settings.public_folder)
+end
