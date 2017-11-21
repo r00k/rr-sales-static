e9cc955c7b2090a540269597e3f47c85ad7b20c3
Ben Orenstein <ben.orenstein@gmail.com>
2017-11-21 10:21:12 -0500

    Add double opt in custom stuff

--- app.rb
+++ app.rb
@@ -7,3 +7,11 @@ end
 get "/" do
   send_file File.expand_path('landing/index.html', settings.public_folder)
 end
+
+get "/thanks" do
+  send_file File.expand_path('landing/thanks.html', settings.public_folder)
+end
+
+get "/confirmed" do
+  send_file File.expand_path('landing/confirmed.html', settings.public_folder)
+end
